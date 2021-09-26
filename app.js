964f4562d59ea134878afab53b4ae8dafc34eab1
HelenaAgustsson <agustssonhelena@gmail.com>
2021-09-26 15:01:32 +0100

    oving1

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import tasks from './data';
+import lists from './lists';
 
 const app = express();
 app.use(express.json());
 
 const PORT = 3000;
+
 app.listen(PORT, () => {
     console.info(`Server running on port ${PORT}`);
 });
@@ -13,10 +15,13 @@ app.get('/api/v1/tasks', (request, response) => {
     response.json(tasks);
 });
 
-app.get('/api/v1/tasks/:id', (request, response) => {
+app.get('/api/v1/lists', (request, response) => {
+    response.json(lists);
+});
+
+app.get('/api/v1/task/:id', (request, response)=>{
     const id = request.params.id;
     const task = tasks.find(t => t.id == id);
-
     if (task) {
         response.json(task);
     } else {
@@ -24,24 +29,49 @@ app.get('/api/v1/tasks/:id', (request, response) => {
     }
 });
 
+app.get('/api/v1/list/:listid', (request, response)=>{
+    const listid = request.params.listid;
+    const list = lists.find(l => l.listid == listid);
+    if (list) {
+        response.json(list);
+    } else {
+        response.status(404).send(`list with id '${id}' not found.`);
+    }
+});
+
 app.post('/api/v1/tasks', (request, response) => {
     const task = request.body;
 
-    if (!task.hasOwnProperty('id') ||
-        !task.hasOwnProperty('title') || 
-        !task.hasOwnProperty('done')) {
-            response.status(400).send('A task needs the following properties: id, title and done.');
+    if(!task.hasOwnProperty('id') || task.hasOwnProperty('title') || task.hasOwnProperty('done') || task.hasOwnProperty('list')) {
+            response.status(400).send('A task needs the following properties: id, title and done');
+        }
+    if(tasks.find(t=>t.id ==task.id)){
+        response.status(400).send(`A task with id '${task.id}' already exists.`);
     }
-
-    if (tasks.find(t => t.id == task.id)) {
-            response.status(400).send(`A task with id '${task.id}' already exists.`);
-    } else {
+    else {
         tasks.push(task);
-      	response.status(201);
-        response.location('tasks/' + task.id);
+        response.status(201);
+        response.location('tasks/'+task.id);
         response.send();
     }
-});
+})
+
+app.post('/api/v1/lists', (request, response) => {
+    const list = request.body;
+
+    if(!list.hasOwnProperty('listid') || list.hasOwnProperty('title')) {
+            response.status(400).send('A list needs the following properties: id, title');
+        }
+    if(lists.find(l=>l.listid ==list.listid)){
+        response.status(400).send(`A list with id '${list.listid}' already exists.`);
+    }
+    else {
+        lists.push(list);
+        response.status(201);
+        response.location('lists/'+list.listid);
+        response.send();
+    }
+})
 
 app.delete('/api/v1/tasks/:id', (request, response) => {
     const id = request.params.id;
@@ -53,3 +83,74 @@ app.delete('/api/v1/tasks/:id', (request, response) => {
         response.status(404).send(`Failed to delete task with id '${id}'. Task not found.`);
     }
 });
+
+app.delete('/api/v1/lists/:listid', (request, response) => {
+    const listid = request.params.listid;
+    const index = lists.findIndex(l => l.listid == listid); 
+    if (index != -1) {
+        lists.splice(index, 1);
+        //remove all tasks with id = list id
+        response.json(lists);
+    } else {
+        response.status(404).send(`Failed to delete list with id '${listid}'. Task not found.`);
+    }
+});
+
+app.get('/api/v1/list/:listid/tasks', (request, response)=>{
+    const listid = request.params.listid;
+    const listTasks = tasks.filter(t => t.list == listid);
+    if (listTasks) {
+        response.json(listTasks);
+    } else {
+        response.status(404).send(`list with id '${listid}' not found.`);
+    }
+});
+
+app.get('/api/v1/list/:listid/tasks/:taskid', (request, response)=>{
+    const listid = request.params.listid;
+    const taskid = request.params.taskid;
+    const listTasks = tasks.filter(t => t.id == taskid);
+    if (listTasks) {
+        response.json(listTasks);
+    } else {
+        response.status(404).send(`list with id '${listid}' not found.`);
+    }
+});
+
+app.post('/api/v1/lists/:listid/tasks', (request, response) => {
+    const task = request.body;
+
+    if(!task.hasOwnProperty('id') || task.hasOwnProperty('title') || task.hasOwnProperty('done')) {
+        response.status(400).send('A task needs the following properties: id, title and done');
+    }
+    if(tasks.find(t=>t.id ==task.id)){
+    response.status(400).send(`A task with id '${task.id}' already exists.`);
+    }
+    else {
+    task.list = request.params.listid;
+    tasks.push(task);
+    response.status(201);
+    response.location('tasks/'+task.id);
+    response.send();
+    }
+})
+
+app.delete('/api/v1/lists/:listid/tasks/:taskid', (request, response) => {
+    const listid = request.params.listid;
+    const taskid = request.params.taskid;
+    const index = tasks.findIndex(t => t.list == listid && t.id == taskid); 
+    if (index != -1) {
+        tasks.splice(index, 1);
+        response.json(tasks);
+    } else {
+        response.status(404).send(`Failed to delete task with id '${taskid}'. Task not found.`);
+    }
+});
+
+/*
+
+
+
+
+
+*/
--- data.js
+++ data.js
@@ -2,19 +2,24 @@ const tasks = [
     {
         id: 1,
         title: "Les leksjon",
-        done: false
+        done: false,
+        list:1
     },
     {
         id: 2,
         title: "Møt opp på forelesning",
-        done: false
+        done: false,
+        list:1
     },
     {
         id: 3,
         title: "Gjør øving",
-        done: false
+        done: false,
+        list:2
     },
    
 ];
+
+
 
 export default tasks;
